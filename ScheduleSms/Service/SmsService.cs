d5d7de9287b11f56f2dc20835036a0e9018194c0
art <art@cowell.com.tw>
2017-12-18 17:56:32 +0800

    sample

---
+++ ScheduleSms/Service/SmsService.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using ScheduleSms.Database;
+
+namespace ScheduleSms.Service
+{
+    internal class SmsService
+    {
+        private IDatabaseConnectionFactory DatabaseConnectionFactory { get; }
+
+        public SmsService(IDatabaseConnectionFactory connectionFactory)
+        {
+            DatabaseConnectionFactory = connectionFactory;
+        }
+
+        public bool CheckSmsEnable()
+        {
+            bool isEnable;
+            using (var sql = DatabaseConnectionFactory.Create())
+            {
+                isEnable = sql.QuerySingle<bool>(GetCheckSmsEnableSqlCmd());
+            }
+            return isEnable;
+        }
+
+        private static string GetCheckSmsEnableSqlCmd()
+        {
+            return "SELECT SMS_ENABLE_FG FROM TEST";
+        }
+    }
+}
